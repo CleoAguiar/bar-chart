50f1f22a0b37db92d8b8f5d9b66e944d6f4e2e99
Cleo Aguiar <cleotaisas@gmail.com>
2019-06-25 10:12:36 -0300

    feat: add Data-Driven Documents (D3)

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,5 @@
-// alert('Data Visualization Project');
+var data = [4, 8, 15, 16, 23, 42];
+
+$(document).ready(function(){
+	d3.select('.barChart').append('p').selectAll('p').data(data).enter().append('p').text((d) => d);
+});
