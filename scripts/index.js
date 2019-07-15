51032fff0042fc3651836e989ed57a9536722e1c
Cleo Aguiar <cleotaisas@gmail.com>
2019-07-15 20:57:50 -0300

    feat: add g element x-axis. User Story #2

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,23 @@
 var data = [4, 8, 15, 16, 23, 42];
+var width = 400, height = 100;
+
+
 
 $(document).ready(function(){
-	d3.select('.barChart').append('p').selectAll('p').data(data).enter().append('p').text((d) => d);
+	// SVG
+	var svg = d3.select('.barChart')
+	.append('svg')
+	.attr('width', width)
+	.attr('height', height);
+
+	// Scale
+	var scale = d3.scaleLinear()
+	.domain([d3.min(data), d3.max(data)])
+	.range([0, width - 100]);
+
+	// Scale to axis
+	var x_axis = d3.axisBottom()
+	.scale(scale);
+
+	svg.append('g').call(x_axis);
 });
