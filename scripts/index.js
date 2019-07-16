60884825b31a3e4c994f9f0bad5f48303869948c
Cleo Aguiar <cleotaisas@gmail.com>
2019-07-15 22:12:10 -0300

    feat: add g element y-axis. User Story #3

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,35 @@
 var data = [4, 8, 15, 16, 23, 42];
-var width = 400, height = 100;
-
-
+var width = 400, height = 400;
 
 $(document).ready(function(){
 	// SVG
 	var svg = d3.select('.barChart')
-	.append('svg')
-	.attr('width', width)
-	.attr('height', height);
+				.append('svg')
+				.attr('width', width)
+				.attr('height', height);
 
 	// Scale
-	var scale = d3.scaleLinear()
-	.domain([d3.min(data), d3.max(data)])
-	.range([0, width - 100]);
+	var xscale = d3.scaleLinear()
+				   .domain([0, d3.max(data)])
+				   .range([0, width - 100]);
 
-	// Scale to axis
+	var yscale = d3.scaleLinear()
+				   .domain([0, d3.max(data)])
+				   .range([height/2, 0]);
+
+	 // Scale to axis
 	var x_axis = d3.axisBottom()
-	.scale(scale);
+				   .scale(xscale);
 
-	svg.append('g').call(x_axis);
+	var y_axis = d3.axisLeft()
+				   .scale(yscale);
+
+	svg.append('g')
+	   .attr('transform', 'translate(50, 10)')
+	   .call(y_axis);
+	
+	var xAxisTranslate = height/2 + 10;
+	svg.append('g')
+	   .attr('transform', 'translate(50, ' + xAxisTranslate  +')')
+	   .call(x_axis);
 });
