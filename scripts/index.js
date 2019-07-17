aff927421cef952c1839361aea83182785559ecb
Cleo Aguiar <cleotaisas@gmail.com>
2019-07-17 11:17:16 -0300

    refact: displaying the data. User Story #4 and #5

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,9 @@
-var data = [1, 2, 4, 7, 12, 20, 33, 54, 88, 143, 232, 376, 609, 986, 1596];
-var width = 400, height = 400;
-
 $(document).ready(function(){
 	
 	// set the dimensions and margins of the graph
-	var margin = {top: 30, right: 30, bottom: 70, left: 100},
-	width = 460 - margin.left - margin.right,
-	height = 400 - margin.top - margin.bottom;
+	var margin = {top: 50, bottom: 400, left: 50, right: 50},
+		width = 660 - margin.left - margin.right,
+		height = 600 - margin.top - margin.bottom;
 
 	// append the svg object to the body of the page
 	var svg = d3.select(".barChart")
@@ -16,73 +13,49 @@ $(document).ready(function(){
 				.append("g")
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 	
+	// var g = svg.append('g')
+	// 		   .attr('transform', 'translate(0,' + height + ')');
+
 	d3.csv('https://raw.githubusercontent.com/CleoAguiar/bar-chart/master/public/dataset.csv').then(function(data){
+	
 		// Sort Data
 		data = data.slice(0, 10);
 		data.sort((b, a) => a.Habitantes - b.Habitantes);
 
+		// Scale to axi X
 		var xscale = d3.scaleBand()
 					   .range([0, width])
-					   .domain(data.map((d) => d.Pais));
-
+					   .domain(data.map((d) => d.Pais))
+					   .padding(.4);
+		
+		svg.append('g')
+		   .attr('transform', 'translate(0,' + height + ')')
+		   .attr('id', 'x-axis')
+		   .call(d3.axisBottom(xscale))
+		   .selectAll('text')
+		   .attr("transform", "translate(-10,0)rotate(-45)")
+		   .style('text-anchor', 'end');
+	
+		// Scale to axi Y
 		var yscale = d3.scaleLinear()
 					   .domain([0, d3.max(data).Habitantes])
 					   .range([height, 0]);
 
-		// Scale to axis
-		var x_axis = d3.axisBottom()
-				       .scale(xscale);
-
-		var y_axis = d3.axisLeft()
-					   .scale(yscale);
-
-		var g = svg.append('g')
-				   .attr('transform', 'translate( 0,' + height + ')');
-		
-		g.append('g')
+		svg.append('g')
 		   .attr('id', 'y-axis')
-		   .call(y_axis);
-			
-		g.append('g')
-		   .attr('id', 'x-axis')
-		   .call(x_axis)
-		   .selectAll("text")
-		   .attr("transform", "translate(-10,0)rotate(-45)")
-		   .style("text-anchor", "end");
+		   .call(d3.axisLeft(yscale)
+					.tickFormat(d => d/1000000 + ' mi')
+					.ticks(5));
+
+		// Bar
+		 svg.selectAll('.bar')
+		   .data(data)
+		   .enter().append('rect')
+		   .attr('class', 'bar')
+		   .attr('x', d => xscale(d.Pais))
+		   .attr('y', d=> yscale(d.Habitantes))
+		   .attr('width', xscale.bandwidth())
+		   .attr('height', d => height - yscale(d.Habitantes));
 	});
 
-	// svg.append('text')
-	//    .attr('x', (width/2))
-	//    .attr('text-anchor', 'middle')
-	//    .text('Sum of Fibonacci Numbers');
-
-	// // Scale
-	// var xscale = d3.scaleLinear()
-	// 			   .domain([0, d3.max(data)])
-	// 			   .range([0, width - 100]);
-
-	// var yscale = d3.scaleLinear()
-	// 			   .domain([0, d3.max(data)])
-	// 			   .range([height/2, 0]);
-
-	//  // Scale to axis
-	// var x_axis = d3.axisBottom()
-	// 			   .scale(xscale);
-
-	// var y_axis = d3.axisLeft()
-	// 			   .scale(yscale);
-
-	// var g = svg.append('g')
-	// 		   .attr('transform', 'translate(' + 10 + ',' + 0 + ')');
-
-	// g.append('g')
-	//    .attr('transform', 'translate(50, 10)')
-	//    .attr('id', 'y-axis')
-	//    .call(y_axis);
-	
-	// var xAxisTranslate = height/2 + 10;
-	// g.append('g')
-	//    .attr('transform', 'translate(50, ' + xAxisTranslate  +')')
-	//    .attr('id', 'x-axis')
-	//    .call(x_axis);
 });
